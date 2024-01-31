574f87bf15f8ddc8d753114259f039f9707d19cb
Efren Ulloa <eulloa345@gmail.com>
2024-01-31 12:02:09 -0600

    feat: header

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,18 +1,52 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export const Header = () => {
   return (
     <header className="p-24">
       <div className="flex">
-        <ul className="flex-1 flex justify-between">
-          <li>Bio</li>
-          <li>News</li>
-          <li>Events</li>
-          <li>CV</li>
+        <ul className="flex-1 flex justify-start">
+          <li className="pr-4">Bio</li>
+          <li className="pr-4">News</li>
+          <li className="pr-4">Events</li>
+          <li>
+            <Link href="/cv.pdf" target="_blank">CV</Link>
+          </li>
         </ul>
-        <ul className="flex-1 flex justify-between">
-          <li>LinkedIn</li>
-          <li>Github</li>
-          <li>Twitter</li>
-          <li>Google Scholar</li>
+        <ul className="flex-1 flex justify-end">
+          <li className="pr-4">
+            <Link
+              href="https://www.linkedin.com/in/mara-ulloa/"
+              target="_blank"
+            >
+              <Image
+                alt="Linked In"
+                src="/img/linkedin.svg"
+                width="25"
+                height="25"
+              />
+            </Link>
+          </li>
+          <li className="pr-4">
+            <Link href="https://github.com/Mara16" target="_blank">
+              <Image
+                alt="Github"
+                src="/img/github.svg"
+                width="25"
+                height="25"
+              />
+            </Link>
+          </li>
+          <li>
+            <Link href="https://twitter.com/mara_ulloa__" target="_blank">
+              <Image
+                alt="Twitter"
+                src="/img/twitter.svg"
+                width="25"
+                height="25"
+              />
+            </Link>
+          </li>
         </ul>
       </div>
     </header>
