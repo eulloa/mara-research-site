d3a31a5ceceeb7807e94c9dfd3d814a42f20fbf1
Efren Ulloa <eulloa345@gmail.com>
2024-02-15 21:40:28 -0600

    feat: events page test

---
+++ src/app/events/page.tsx
@@ -0,0 +1,44 @@
+import fs from "fs";
+import path from "path";
+
+import { Events } from "@/components";
+
+const getImages = () => {
+  const dir = path.resolve("./public/img/events");
+  const images = fs.readdirSync(dir);
+
+  return images;
+};
+
+const getDescriptions = () => {
+  const descriptions: string[] = [];
+
+  const captions = fs.readdirSync("./public/img/captions/", {
+    encoding: "utf-8",
+  });
+
+  for (let i = 0; i < captions.length; i++) {
+    const data = fs.readFileSync(`./public/img/captions/${captions[i]}`, {
+      encoding: "utf-8",
+    });
+
+    descriptions.push(data);
+  }
+
+  return descriptions;
+};
+
+export default function Home() {
+  const descriptions = getDescriptions();
+
+  const images = getImages().map((img, index) => ({
+    description: descriptions[index],
+    src: `/img/events/${img}`,
+  }));
+
+  return (
+    <>
+      <Events images={images} />
+    </>
+  );
+}
