826583d3f84480d6361928eee8ed78b4c7c6ab02
Efren Ulloa <eulloa345@gmail.com>
2024-02-19 14:54:55 -0600

    feat: optimize mobile nav at lg breakpoint

--- src/app/components/nav/navSmall.tsx
+++ src/app/components/nav/navSmall.tsx
@@ -17,36 +17,41 @@ export const NavSmall = ({ onClick }: TNavSmall) => {
   return (
     <nav
       style={{ top: `${top}px` }}
-      className="absolute md:flex bottom-0 right-0 left-0 md:static dark:text-white align-middle bg-eggshell dark:bg-navy h-screen z-10"
+      className="absolute bottom-0 right-0 left-0 dark:text-white align-middle bg-eggshell dark:bg-navy h-screen z-10"
     >
       <div className="relative top-1/2 translate-y-[-50%]">
-        <ul className="md:flex-1 md:flex justify-start mb-20">
-          <li className="md:pr-8 mb-8">
-            <a className="flex justify-center" href="#news">
-              <button onClick={onClick}>News</button>
+        <ul className="justify-center mb-20">
+          <li className="mb-8">
+            <a className="flex justify-center" href="#news" onClick={onClick}>
+              News
             </a>
           </li>
-          <li className="md:pr-8 mb-8">
-            <a className="flex justify-center" href="#publications">
-              <button onClick={onClick}>Publications</button>
+          <li className="mb-8">
+            <a
+              className="flex justify-center"
+              href="#publications"
+              onClick={onClick}
+            >
+              Publications
             </a>
           </li>
-          <li className="md:pr-8 mb-8">
-            <a className="flex justify-center" href="#events">
-              <button onClick={onClick}>Events</button>
+          <li className="mb-8">
+            <a className="flex justify-center" href="#events" onClick={onClick}>
+              Events
             </a>
           </li>
           <li>
             <Link
               className="hover:underline block text-center"
               href="/cv.pdf"
+              onClick={onClick}
               target="_blank"
             >
-              <button onClick={onClick}>CV</button>
+              CV
             </Link>
           </li>
         </ul>
-        <ul className="mt-4 flex-1 flex justify-center md:justify-end">
+        <ul className="mt-4 flex-1 flex justify-center">
           <li className="pr-8">
             <Link
               href="https://www.linkedin.com/in/mara-ulloa/"
