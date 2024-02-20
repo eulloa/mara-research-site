657b0180b29d0d082f2348925a54633b3243a8ae
Efren Ulloa <eulloa345@gmail.com>
2024-02-20 17:09:13 -0600

    refactor: cv clicked event

--- src/app/components/nav/navLarge.tsx
+++ src/app/components/nav/navLarge.tsx
@@ -34,7 +34,7 @@ export const NavLarge = () => {
             className="hover:underline"
             href="/cv.pdf"
             target="_blank"
-            onClick={() => sendGAEvent({ event: "CVClicked" })}
+            onClick={() => sendGAEvent({ event: "cv_clicked", value: true })}
           >
             CV
           </Link>
