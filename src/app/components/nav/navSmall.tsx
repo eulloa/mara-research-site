1e3e3bea0961b7395d81d9331e19300225b83577
Efren Ulloa <eulloa345@gmail.com>
2025-02-14 19:14:40 -0600

    feat: remove address + update cv_clicked

--- src/app/components/nav/navSmall.tsx
+++ src/app/components/nav/navSmall.tsx
@@ -44,10 +44,7 @@ export const NavSmall = ({ onClick }: TNavSmall) => {
               href="/cv.pdf"
               onClick={() => {
                 onClick();
-                sendGAEvent("click", "cv_clicked", {
-                  event: "select_content",
-                  value: "CV",
-                });
+                sendGAEvent("click", "cv_clicked");
               }}
               target="_blank"
             >
