224fe83f85434ccaac7535f123be089880f7b503
Efren Ulloa <eulloa345@gmail.com>
2025-03-29 00:51:02 -0500

    refactor: news

---
+++ src/app/components/newsItems/newsItems.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+import {Bar} from "./bar";
+import {Baz} from "./baz";
+import {Foo} from "./foo";
+
+export const newsItems = {
+    "stateOne": <Bar />,
+    "stateTwo": <Baz />,
+    "stateThree": <Foo />
+};
+
